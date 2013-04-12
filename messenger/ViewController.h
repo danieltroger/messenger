04b2f35a4d2f9578a432e95e8a88e54cc7803ad0
Daniel Troger <daniel@natur-kultur.eu>
2013-04-12 21:07:47 +0200

    Mail again...

--- messenger/ViewController.h
+++ messenger/ViewController.h
@@ -7,7 +7,20 @@
 //
 
 #import <UIKit/UIKit.h>
-
+//NSString *phpScript;
+//NSURL *phpURL;
+//NSString *content;
 @interface ViewController : UIViewController
+{
+    IBOutlet UITextField *from;
+    IBOutlet UITextField *sub;
+    IBOutlet UITextField *to;
+    IBOutlet UILabel *extras;
+    IBOutlet UITextView *message;
+    IBOutlet UIButton *sendbut;
+    IBOutlet UIButton *hidebut;
+}
+ -(IBAction) send;
+ -(IBAction) hide;
 
 @end
--- messenger/ViewController.m
+++ messenger/ViewController.m
@@ -12,8 +12,44 @@
 
 @end
 
-@implementation ViewController
-
+@implementation ViewController  
+ -(IBAction) hide
+{
+    [from resignFirstResponder];
+    [to resignFirstResponder];
+    [message resignFirstResponder];
+    [sub resignFirstResponder];
+}
+-(IBAction) send
+{
+    NSString *fromc = (NSString *)CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(NULL,(__bridge CFStringRef) from.text, NULL,CFSTR("!*'();:@&=+$,/?%#[]"),kCFStringEncodingUTF8));
+     NSString *toc = (NSString *)CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(NULL,(__bridge CFStringRef) to.text, NULL,CFSTR("!*'();:@&=+$,/?%#[]"),kCFStringEncodingUTF8));
+     NSString *msgc = (NSString *)CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(NULL,(__bridge CFStringRef) message.text, NULL,CFSTR("!*'();:@&=+$,/?%#[]"),kCFStringEncodingUTF8));
+     NSString *subc = (NSString *)CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(NULL,(__bridge CFStringRef) sub.text, NULL,CFSTR("!*'();:@&=+$,/?%#[]"),kCFStringEncodingUTF8));
+    NSString *phpScript = [NSString stringWithFormat: @"http://www.dt.comyr.com/sendmail.php?from=%@&to=%@&msg=%@&sub=%@",fromc,toc,msgc,subc];
+   /* phpScript = [phpScript stringByReplacingOccurrencesOfString:@"\n"
+                                                     withString:@"%0D%0A"];*/
+    
+        NSLog(@"%@",phpScript);
+    NSURL *phpURL = [NSURL URLWithString:phpScript];
+    NSString *content = [NSString stringWithContentsOfURL:phpURL encoding:NSASCIIStringEncoding error:nil];
+    UIAlertView *al = [UIAlertView alloc];
+    [al initWithTitle:@"" message:@"Success!" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+    [al show];
+   /* if([content isEqualToString:@"1"])
+    {
+        UIAlertView *al = [UIAlertView alloc];
+        [al initWithTitle:@"" message:@"Success!" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+        [al show];
+    }
+    else
+    {
+        UIAlertView *al = [UIAlertView alloc];
+        [al initWithTitle:@"" message:@"Failure" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+        [al show];
+    }*/
+    NSLog(@"%@",content);
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
